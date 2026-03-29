7a27f98ed1f6f75560a548ab62459d8faf228cc0
testdoliet <contagamer5903@gmail.com>
2026-03-29 06:43:20 -0300

    Update superflixapi.js

--- providers/superflixapi.js
+++ providers/superflixapi.js
@@ -10,19 +10,16 @@ let SESSION_DATA = {
     pageToken: ''
 };
 
-private val HEADERS = mapOf(
-    "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-    "Accept-Language" to "pt-BR,pt;q=0.9,en;q=0.8",
-    "Referer" to "https://lospobreflix.site/",
-    "Sec-Fetch-Dest" to "document",  // Mudar de 'iframe' para 'document'
-    "Sec-Fetch-Mode" to "navigate",
-    "Sec-Fetch-Site" to "cross-site",
-    "Upgrade-Insecure-Requests" to "1",
-    "Connection" to "keep-alive",
-    "Cache-Control" to "no-cache",   // Adicionar
-    "Pragma" to "no-cache"            // Adicionar
-)
+const HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Mobile Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'pt-BR',
+    'Referer': 'https://lospobreflix.site/',
+    'Sec-Fetch-Dest': 'iframe',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'cross-site',
+    'Upgrade-Insecure-Requests': '1',
+    'Connection': 'keep-alive'
 };
 
 const API_HEADERS = {
